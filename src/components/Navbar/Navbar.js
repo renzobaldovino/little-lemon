2a6098986b86f030178f0938b9b4659b5ef945a3
renzobaldovino <renzo.baldovino@gmail.com>
2023-05-29 17:07:25 +0800

    feat(component): :sparkles: create nav component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import "./navbar.css";
+import logo from "../../assets/Logo.svg";
+
+import { NAVLINKS } from "../../utils/navLinks";
+import { Link, useLocation } from "react-router-dom";
+
+const Navbar = () => {
+	const loc = useLocation();
+
+	return (
+		<header>
+			<nav>
+				<div className='logo'>
+					<img src={logo} alt='Little Lemon Logo' />
+				</div>
+				<ul className='nav-links'>
+					{NAVLINKS.map((nav) => (
+						<li key={nav.name}>
+							<Link
+								to={nav.path}
+								className={
+									loc.pathname === nav.path ? "active" : ""
+								}
+							>
+								{nav.name}
+							</Link>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</header>
+	);
+};
+
+export default Navbar;
