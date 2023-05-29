a0a07ed0bb4610eaf18d55a9307dda5cdec927fa
renzobaldovino <renzo.baldovino@gmail.com>
2023-05-29 20:33:24 +0800

    feat(component): :sparkles: create component for hero section

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,25 @@
+import "./hero.css";
+import HeroImage from "../../assets/hero-image.jpg";
+
+const Hero = () => {
+	return (
+		<section className='hero'>
+			<div className='content'>
+				<div className='spiel'>
+					<h1>Little Lemon</h1>
+					<h2>Chicago</h2>
+					<p>
+						We are a family owned Mediterranean restaurant, focused
+						on traditional recipes served with a modern twist.
+					</p>
+					<button className="btn">Reserve a Table</button>
+				</div>
+				<div className='img-container'>
+					<img src={HeroImage} />
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Hero;
