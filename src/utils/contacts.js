4d318e22fb54719faff88c7cdf03a76d6c24f77e
renzobaldovino <renzo.baldovino@gmail.com>
2023-05-30 18:29:37 +0800

    feat(component): :sparkles: create data for footer section

---
+++ src/utils/contacts.js
@@ -0,0 +1,16 @@
+import { FaMapPin, FaPhoneAlt, FaEnvelope } from "react-icons/fa";
+
+export const CONTACTS = [
+  {
+    icon: <FaMapPin className="icon" />,
+    details: "Address"
+  },
+  {
+    icon: <FaPhoneAlt className="icon" />,
+    details: "Phone"
+  },
+  {
+    icon: <FaEnvelope className="icon" />,
+    details: "Email"
+  }
+];
---
+++ src/utils/socialMedia.js
@@ -0,0 +1,20 @@
+import { FaFacebook, FaTwitter, FaInstagram, FaYoutube } from "react-icons/fa";
+
+export const SOCIAL_MEDIA = [
+	{
+		link: "https://www.facebook.com",
+		icon: <FaFacebook className='icon' />,
+	},
+	{
+		link: "https://www.twitter.com",
+		icon: <FaTwitter className='icon' />,
+	},
+	{
+		link: "https://www.instagram.com",
+		icon: <FaInstagram className='icon' />,
+	},
+	{
+		link: "https://www.youtube.com",
+		icon: <FaYoutube className='icon' />,
+	},
+];
