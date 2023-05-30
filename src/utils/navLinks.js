ac904f496b9c4d2b60a3a15e5ef11a5c3d6a8f20
renzobaldovino <renzo.baldovino@gmail.com>
2023-05-30 18:30:56 +0800

    feat(component): :sparkles: create data for nav links

---
+++ src/utils/navLinks.js
@@ -0,0 +1,26 @@
+export const NAVLINKS = [
+	{
+		name: "Home",
+		path: "/",
+	},
+	{
+		name: "About",
+		path: "/about",
+	},
+	{
+		name: "Menu",
+		path: "/menu",
+	},
+	{
+		name: "Reservations",
+		path: "/bookings",
+	},
+	{
+		name: "Order Online",
+		path: "/order",
+	},
+	{
+		name: "Login",
+		path: "/login",
+	},
+];
