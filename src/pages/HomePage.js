303233485b2742bd337185c445bc6de3a8c9443d
renzobaldovino <renzo.baldovino@gmail.com>
2023-05-30 18:33:55 +0800

    feat(component): :sparkles: create home page

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import Hero from "../components/Hero/Hero";
+import Highlights from "../components/Highlights/Highlights";
+import Testimonials from "../components/Testimonials/Testimonials";
+import About from "../components/About/About";
+import Footer from "../components/Footer/Footer";
+
+const HomePage = () => {
+	return (
+		<>
+			<Hero />
+			<Highlights />
+			<Testimonials />
+			<About />
+		</>
+	);
+};
+
+export default HomePage;
