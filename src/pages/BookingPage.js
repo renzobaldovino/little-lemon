55d594c73041a202b95b4e74f98edbf1859ec272
renzobaldovino <renzo.baldovino@gmail.com>
2023-06-01 18:30:19 +0800

    feat(component): :sparkles: create booking page

---
+++ src/pages/BookingPage.js
@@ -0,0 +1,37 @@
+import { useReducer } from "react";
+import BookingForm from "../components/BookingForm/BookingForm";
+import { fetchAPI, submitAPI } from "../utils/api";
+
+const updateTimes = (availableTimes, date) => {
+	const res = fetchAPI(new Date(date));
+	return res.length !== 0 ? res : availableTimes;
+};
+
+const initializeTimes = (initialAvailableTimes) => [
+	...initialAvailableTimes,
+	...fetchAPI(new Date()),
+];
+
+const BookingPage = () => {
+	const [availableTimes, dispatch] = useReducer(
+		updateTimes,
+		[],
+		initializeTimes
+	);
+
+	const submitData = (formData) => {
+		const res = submitAPI(formData);
+	};
+
+	return (
+		<>
+			<BookingForm
+				availableTimes={availableTimes}
+				dispatch={dispatch}
+				submitData={submitData}
+			/>
+		</>
+	);
+};
+
+export default BookingPage;
