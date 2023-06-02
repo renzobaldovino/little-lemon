e80447c614018eb4e212f1c689d8685ef72821d6
renzobaldovino <renzo.baldovino@gmail.com>
2023-06-02 15:39:40 +0800

    feat(component): :coffin: remove import for footer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,6 @@ import Hero from "../components/Hero/Hero";
 import Highlights from "../components/Highlights/Highlights";
 import Testimonials from "../components/Testimonials/Testimonials";
 import About from "../components/About/About";
-import Footer from "../components/Footer/Footer";
 
 const HomePage = () => {
 	return (
