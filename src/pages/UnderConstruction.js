70f86ea4c938bd0a9ef5f6f87087ac13493ae7d0
renzobaldovino <renzo.baldovino@gmail.com>
2023-06-02 15:39:00 +0800

    feat(component): :sparkles: create under construction page

---
+++ src/pages/UnderConstruction.js
@@ -0,0 +1,14 @@
+import { FaTools } from "react-icons/fa";
+
+const UnderConstruction = () => {
+	return (
+		<section className='full'>
+			<div>
+				<FaTools className='icon' />
+				<h1>Page Under Construction</h1>
+			</div>
+		</section>
+	);
+};
+
+export default UnderConstruction;
