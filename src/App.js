f374c3e2a1ea887b0618170fde7ba19d94fbf927
renzobaldovino <renzo.baldovino@gmail.com>
2023-06-02 15:40:26 +0800

    feat(route): :sparkles: add routing for confirmed booking, under construction, and not found

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import HomePage from "./pages/HomePage";
 import BookingPage from "./pages/BookingPage";
+import ConfirmedBookingPage from "./pages/ConfirmedBookingPage";
+import UnderConstruction from "./pages/UnderConstruction";
+import NotFound from "./pages/NotFound";
 
 function App() {
 	return (
@@ -11,6 +14,15 @@ function App() {
 			<Routes>
 				<Route path='/' element={<HomePage />} />
 				<Route path='/bookings' element={<BookingPage />} />
+				<Route
+					path='/confirmed-booking'
+					element={<ConfirmedBookingPage />}
+				/>
+				<Route path='/about' element={<UnderConstruction />} />
+				<Route path='/menu' element={<UnderConstruction />} />
+				<Route path='/order' element={<UnderConstruction />} />
+				<Route path='/login' element={<UnderConstruction />} />
+				<Route path='*' element={<NotFound />} />
 			</Routes>
 			<Footer />
 		</BrowserRouter>
